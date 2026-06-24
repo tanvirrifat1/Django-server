06ff0bd9ffa95c5a07b26d8498acbaa2def3ee2f
tanvirrifat1 <rifatkhan5567790@gmail.com>
2026-06-24 21:12:11 +0600

    added templates

--- course/views.py
+++ course/views.py
@@ -5,4 +5,14 @@ from django.http import HttpResponse
 
 
 def free_course(request):
-    return render(request, "free_course.html")
+    course1 = "free course"
+    course2 = "paid course"
+    course3 = "free course"
+    course4 = "paid course"
+    course_details = {
+        "c1": course1,
+        "c2": course2,
+        "c3": course3,
+        "c4": course4,
+    }
+    return render(request, "resources/free_course.html", course_details)
