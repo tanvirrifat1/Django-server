371b0039f2a678ab0aaa1794116dc38e908fd5b4
tanvirrifat1 <rifatkhan5567790@gmail.com>
2026-07-01 21:22:39 +0600

    added

--- core/urls.py
+++
@@ -1,9 +0,0 @@
-from django.contrib import admin
-from django.urls import path
-from django.urls import include
-
-urlpatterns = [
-    path("admin/", admin.site.urls),
-    path("cr/", include("course.urls")),
-    path("res/", include("resources.urls")),
-]
--- resources/urls.py
+++
@@ -1,6 +0,0 @@
-from django.urls import path
-from . import views
-
-urlpatterns = [
-    path("fr", views.free_course),
-]
